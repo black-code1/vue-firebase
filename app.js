70748f5e59b7a21388e59e9f2919517418907e90
black-code1 <legrandmahamat1@gmail.com>
2022-03-18 14:29:04 +0100

    Templates & Data

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
-  // data, functions
-  //   template: "<h2>I am the template</h2>",
+  data() {
+    return {
+      title: "The final empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
 });
 
 app.mount("#app");
